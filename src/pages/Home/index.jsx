e24230e7d388807f9480bfea8f7f942ec9c8d921
Keller <Keller2007@rambler.ru>
2023-02-13 22:56:28 +0300

    first commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
+// import t from './img/1.jpg';
+// import { gsap } from 'gsap';
+// import '../../libs/gsap.min.js';
+// import '../../libs/Draggable.min.js';
+// import '../../libs/InertiaPlugin.min.js';
 
 export const Home = () => {
+  const [state, setState] = React.useState('Loading...');
+  React.useEffect(() => {
+    return () => {
+      setState('Федор Шехтель');
+    };
+  }, []);
+  const t = './img/1.jpg';
   return (
-	<div>Home</div>
-  )
-}
+    <div className="wrapper">
+      <h1>АРХИТЕКТОР</h1>
+      <div>{state}</div>
+      <div className="galllery">
+        <div className="gallery__item">
+          <img src={t} alt="1" />
+        </div>
+      </div>
+    </div>
+  );
+};
